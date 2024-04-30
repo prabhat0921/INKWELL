3ba384c27da4013e2b5cf9082ffc67e33cbfce5d
prabhat0921 <91479933+prabhat0921@users.noreply.github.com>
2024-04-30 12:05:58 +0530

    commit

--- scripts.js
+++ scripts.js
@@ -14,21 +14,5 @@ $(document).ready(function() {
     }
   });
 
-  // Dynamically load product cards
-  // You can fetch product data from a server and generate cards dynamically
-  // For demonstration purposes, I'll just create a sample card
-  var productCard = '<div class="col-md-4 mb-4">' +
-                      '<div class="card">' +
-                        '<img src="notebook.jpg" class="card-img-top" alt="Notebook">' +
-                        '<div class="card-body">' +
-                          '<h5 class="card-title">Premium Notebook</h5>' +
-                          '<p class="card-text">High-quality notebook for professionals.</p>' +
-                          '<a href="product-details.html" class="btn btn-primary">View Details</a>' +
-                        '</div>' +
-                      '</div>' +
-                    '</div>';
+})
 
-  // Append product cards to the products section
-
-
-  // Form submission handling
